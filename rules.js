3313aa584837193b5ed1cd8a475475f54b451ba9
DevMetal00 <42822581+OctaEDLP00@users.noreply.github.com>
2023-11-27 18:05:16 -0300

    Actualizar

---
+++ rules.js
@@ -0,0 +1,12 @@
+"use strict";
+var RULES;
+(function (RULES) {
+    RULES["OFF"] = "off";
+    RULES["ON"] = "on";
+    RULES["ERROR"] = "error";
+    RULES["WARN"] = "warn";
+    // Add rule
+    /*
+     * RULES[RULE] = "";
+     */
+})(RULES || (RULES = {}));
